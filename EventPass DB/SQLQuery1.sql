1f239de660806f8bff3a8324516e6672d5a4b7bb
LEANDRO JOE JALDIN GUTIERREZ <leandro.jaldin@ucb.edu.bo>
2026-06-08 23:14:51 -0400

    Subiendo base de datos SQL Server

---
+++ EventPass DB/SQLQuery1.sql
@@ -0,0 +1,20 @@
+Create Database EventPassDB;
+Go
+
+USE EventPassDB;
+GO
+
+CREATE TABLE Eventos (
+    Id INT IDENTITY(1,1) PRIMARY KEY, -- IDENTITY hace que el ID sea autoincrementable
+    Nombre NVARCHAR(100),
+    Ubicacion NVARCHAR(100),
+    PrecioEntrada FLOAT
+);
+GO
+
+
+-- Insertamos un par de datos de prueba para que no esté vacía
+INSERT INTO Eventos (Nombre, Ubicacion, PrecioEntrada) 
+VALUES ('Concierto Rock', 'Campus UCB', 150.0),
+       ('Feria de Tecnología', 'Pabellón A', 50.0);
+GO
